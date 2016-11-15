cb035ce5a23e68534312917b723c720f7141e474
holmberg18 <holmbergj25@gmail.com>
2016-11-15 02:51:48 +0000

    finished

--- public/main.js
+++ public/main.js
@@ -1,4 +1,39 @@
 var pictionary = function(socket) {
+    
+    var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
+    
+    var guessBox;
+
+var onKeyDown = function(event) {
+    if (event.keyCode != 13) { // Enter
+        return;
+    }
+
+    console.log(guessBox.val());
+    var guess = guessBox.val();
+    socket.emit('guess', guess);
+    guessBox.val('');
+    
+    
+};
+
+guessBox = $('#guess input');
+guessBox.on('keydown', onKeyDown);
+
     var canvas, context;
 
     var draw = function(position) {
@@ -12,30 +47,83 @@ var pictionary = function(socket) {
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
-    canvas.on('mousemove', function(event) {
+    
+    var drawing = false;
+    
+    canvas.on('mousedown', function(){
         
-        var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
+       drawing = true; 
         
-        socket.emit('draw',position);
-       
     });
+    
+    canvas.on('mouseup',function(){
+        
+        drawing = false;
+    })
+
+var firstDrawer = false;
+var guesser = false;
+var randWord = Math.floor(Math.random() * (WORDS.length - 1));
+
+socket.on("drawer", function(first){
+    context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+       if(first == true){
+            $('#top-message').hide();
+            $('#draw-word').show();
+           
+            console.log(randWord);
+            $('#draw-word').html(WORDS[randWord]);
+        } else {
+            $('#top-message').show();
+        }
+        
+    if(first == true){
+        firstDrawer = true;
+    }//end if first true
+    
+     else{ 
+         guesser = true;
+    }
+});
+
+canvas.on('mousemove', function(event) {
+             if (drawing == true && firstDrawer == true){
+                    // $('.top-message')
+                    var offset = canvas.offset();
+                    var position = {x: event.pageX - offset.left,
+                                    y: event.pageY - offset.top};
+                    
+                    socket.emit('draw',position);
+             }
+        });
+
+    
     
      socket.on("draw", function(position){
         draw(position);
-    })
-};
+    });
+    
+socket.on("guess", function(guess){
+     
+        console.log("A guess is made!!!!!!!!!!");
+        $('.guess-value').html(guess);
+        
+        if(guess == WORDS[randWord]){
+            socket.emit('correct-answer', ''+guess+' is correct');
+        }
+     
+});
+
+socket.on('answer', function(answer){
+    console.log(answer);
+    $('.guess-value').html(answer); 
+    
+});
+
+}
 
 jQuery(document).ready(function() {
     var socket = io();
     pictionary(socket);
-   
-    
-   
-    
-    // socket.on("draw", function(position){
-    //     pictionary(socket);
-    // })
     
 });
--- server.js
+++ server.js
@@ -7,13 +7,54 @@ app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var lastGuesser = '';
+
+var clients = {
+        // instantiate empty object
+}
 
 io.on('connection', function (socket) {
-       
+        
+        clients[socket.id]= socket.id;
+        console.log(clients);
+        console.log(Object.keys(clients).length);
+         if(Object.keys(clients).length == 1){
+               socket.emit("drawer", true);  
+         }
+         else{
+                 socket.emit("drawer", false);
+         }
+     
+     
+     
+     
         socket.on("draw", function(position){
              io.sockets.emit("draw",position);
         });
+
+        socket.on("guess", function(guess){
+                lastGuesser = socket.id;
+                // console.log("a guess was made");
+                socket.broadcast.emit('guess', guess);     
+        });
+        
+        socket.on("disconnect",function(){
+        
+                delete clients[socket.id];  
+        
+});
+        socket.on('correct-answer', function(correctAnswer){
+            
+            io.sockets.emit('answer',correctAnswer);
+            io.sockets.to(lastGuesser).emit('drawer',true);
+            socket.emit('drawer',false)
+            
+        });
+        
+        
+    
     });
     
+
 
 server.listen(process.env.PORT || 8080);
